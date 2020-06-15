21db5789761cc5bd33db519c968f1d2d2e6c2c4d
15butleroa <62599574+15butleroa@users.noreply.github.com>
2020-06-15 22:37:17 +0100

    Update Circle.cs

--- Circle.cs
+++ Circle.cs
@@ -1,47 +1,51 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WhatsTheDiameter
+
 {
     class Circle
     {
         private double radius;
-        // You can use Math.PI to get pi.
-
         public Circle(double radius)
         {
             this.radius = radius;
         }
 
-        /// <summary>
-        /// Should give me the radius d'oh
-        /// </summary>
-        /// <returns>THE RADIUS</returns>
+
+
         public double getRadius()
+
         {
             return this.radius;
         }
 
-        public void setRadius(double r)
-        {
-            this.radius = r;
-        }
-
         override public String ToString()
+
         {
             return this.radius.ToString();
         }
-
-        // Need now getDiameter, getCircumference, getArea.
 
         public double getDiameter()
         {
             return this.radius * 2;
         }
+        public double getCircumference()
+        {
+            return this.getDiameter() * Math.PI;
+        }
+        public double getArea()
+        {
+            return Math.Pow(Math.PI * this.getRadius(), 2);
+        }
 
-        // Neet also setRadius.
+        public void setRadius(double newRadius)
+
+        {
+            this.radius = newRadius;
+        }
     }
 }
