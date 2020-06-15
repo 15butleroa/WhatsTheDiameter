16aabde59319cfbde8f86b8c3fcbcc348cab5b77
15butleroa <62599574+15butleroa@users.noreply.github.com>
2020-06-15 22:44:18 +0100

    Update Form1.cs

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,27 +9,22 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WhatsTheDiameter
+
 {
     public partial class Form1 : Form
-    {        
-        /// <summary>
-        /// This must be kept in sync with CircleCollection.
-        /// </summary>
+    {
         private List<Circle> circles;
         public Form1()
         {
-            InitializeComponent();
 
-            this.circles = new List<Circle>();
+            InitializeComponent();
+            this.circles = List<Circle>();
+
         }
+
+
 
         private void NewCircle_Click(object sender, EventArgs e)
-        {
-            var circle = new Circle(Convert.ToDouble(CircleInput.Text));
-            this.circles.Add(circle);
-            this.RedrawCircleCollection();
-        }
-
         private void RedrawCircleCollection()
         {
             CircleCollection.Items.Clear();
@@ -41,35 +36,27 @@ namespace WhatsTheDiameter
 
         private void ShowArray_Click(object sender, EventArgs e)
         {
-            String msg;
-
-            msg =  "Radius    Diameter\n";
-            msg += "---------------------\n";
+            string msg = "Radius         Diameter\n";
+            msg +=       "---------------------------\n";
             foreach (var c in this.circles)
             {
-                msg += String.Format("{0}    {1}\n", c.getRadius(), c.getDiameter());
+                msg += String.Format("{0}                        {1}\n", c.getRadius(), c.getDiameter());
             }
-
-            // TODO Set to monospaced font so is properly aligned.
-
-            MessageBox.Show(msg);
+            MessageBox.Show(msg); 
         }
+
 
         private void SetRadius_Click(object sender, EventArgs e)
         {
-            double newRadius = Convert.ToDouble(CircleInput.Text);
-            var c = circles[CircleCollection.SelectedIndex];
+                double newRadius = Convert.ToDouble(CircleInput.Text);
+                var c=circles[CircleCollection.SelectedIndex]; 
+                c.setRadius(newRadius);
+                this.RedrawCircleCollection();
 
-            c.setRadius(newRadius);
-            this.RedrawCircleCollection();
+            }
+            catch (System.FormatException)
+            {
+
+                MessageBox.Show("Radius format incorrect.");
+            }
         }
-
-        private void CircleCollection_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            var c = circles[CircleCollection.SelectedIndex];
-
-            CircleData.Text = String.Format("Radius: {0}\nDiameter: {1}", c.getRadius(), c.getDiameter());
-        }
-
-    }
-}
